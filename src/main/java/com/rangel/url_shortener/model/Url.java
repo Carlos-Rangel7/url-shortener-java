9047f64c1783fd131b05b000d2d91ee2c4f0dcb9
Carlos Rangel <croi7726@gmail.com>
2025-03-14 18:21:25 -0300

    Criação da classe Url e configuração inicial do projeto

---
+++ src/main/java/com/rangel/url_shortener/model/Url.java
@@ -0,0 +1,35 @@
+package com.rangel.url_shortener.model;
+
+public class Url {
+
+    private String originalUrl;
+    private String shortenedUrl;
+
+    public Url() {
+    }
+
+    public Url(String originalUrl, String shortenedUrl) {
+        this.originalUrl = originalUrl;
+        this.shortenedUrl = shortenedUrl;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortenedUrl() {
+        return shortenedUrl;
+    }
+
+    public void setShortenedUrl(String shortenedUrl) {
+        this.shortenedUrl = shortenedUrl;
+    }
+
+
+
+
+}
