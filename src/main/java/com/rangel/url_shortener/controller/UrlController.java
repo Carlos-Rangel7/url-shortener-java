499e1493a0ceed4b8da599f096a1fdbfb88d5a5a
Carlos Rangel <croi7726@gmail.com>
2025-03-14 23:46:58 -0300

    Implementação do Controller para endpoints de encurtamento e recuperação de URLs

---
+++ src/main/java/com/rangel/url_shortener/controller/UrlController.java
@@ -0,0 +1,40 @@
+package com.rangel.url_shortener.controller;
+
+import com.rangel.url_shortener.model.Url;
+import com.rangel.url_shortener.service.UrlService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/urls")
+public class UrlController {
+
+    private UrlService service;
+
+    public UrlController(UrlService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/shorten")
+    public Url shortenedUrl(@RequestBody Url url) {
+        return service.shortenUrl(url);
+    }
+
+    public ResponseEntity<Void> redirectToOriginalUrl(@PathVariable String shortenedCode) {
+        String originalUrl = service.getOriginalUrl(shortenedCode);
+
+        if(originalUrl != null) {
+            return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(originalUrl)).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+    }
+
+
+
+
+}
